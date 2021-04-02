d734ed77580f9eb391b98a2d4d24bba5366ea312
Tariq Khan <45430120+Thk101@users.noreply.github.com>
2021-04-02 18:55:53 -0400

    updated

---
+++ pages/patient/profile/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PatientProfile() {
+  return <div>Patient Profile</div>;
+}
+
+export default PatientProfile;
---
+++ pages/signin/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div>Sign in</div>;
+}
+
+export default index;
