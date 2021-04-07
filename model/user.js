60b739721103e23e4a28972a7a2fe2d4983b1366
Lushi <vankovc@gmail.com>
2021-04-07 19:45:22 -0400

    added user, daily tip and emergency tip schema

--- model/user.js
+++ model/user.js
@@ -1,26 +1,28 @@
 import mongoose from 'mongoose';
-var Schema = mongoose.Schema;
 
-var user = new Schema({
-  firstName: {
-    type: String
-  },
-  lastName: {
-      type: String
-  },
-  email: {
-    type: String
-  },
-  password: {
-    type: String
-  },
-  role: {
-    type: String,
-  }
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    role: {
+        type: String,
+        enum: ["nurse", "patient"]
+    }
 });
 
 mongoose.models = {};
 
-var User = mongoose.model('User', user);
+const User = mongoose.model('User', user);
 
 export default User;
