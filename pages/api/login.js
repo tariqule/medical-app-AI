20ef43f231964279363a423862bce9ca4b5f022b
Lushi <vankovc@gmail.com>
2021-04-11 12:26:50 -0400

    added JWT authentication for register and login

---
+++ pages/api/login.js
@@ -0,0 +1,47 @@
+import connectDB from '../../middleware/mongodb';
+import User from '../../model/user';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from '../../config/development';
+import { serialize } from 'cookie';
+const jwtKey = config.secretKey;
+const jwtExpirySeconds = 300;
+
+const handler = async (req, res) => {
+  if (req.method === 'POST') {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        console.log('unable to find user');
+        res.status(500).send(err);
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          // Create a new token with the user id in the payload
+          // and which expires 300 seconds after issue
+          const token = jwt.sign({ id: user._id, username: user.email }, jwtKey,
+            { algorithm: 'HS256', expiresIn: jwtExpirySeconds });
+          // set the cookie as the token string, with a similar max age as the token
+          // here, the max age is in milliseconds
+          // res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000, httpOnly: true });
+          res.setHeader('Set-Cookie', serialize('token', token, { maxAge: jwtExpirySeconds * 1000, httpOnly: true }));
+          res.status(200).send({ screen: user.username, _id: user._id });
+          console.log('logged in');
+          req.user = user;
+          //call the next middleware
+          // next();
+        } else {
+          res.json({
+            status: "error", message: "Invalid username/password.",
+            data: null
+          });
+        }
+      }
+    });
+  } else {
+    res.status(422).send('req_method_not_supported');
+  }
+}
+
+export default connectDB(handler);
--- pages/api/register.js
+++ pages/api/register.js
@@ -1,36 +1,59 @@
 import connectDB from '../../middleware/mongodb';
 import User from '../../model/user';
 
-const handler = async (req, res) => {
-    const saltRounds = 10;
+// const handler = async (req, res) => {
+//     const saltRounds = 10;
 
+//     if (req.method === 'POST') {
+//         // Check if name, email or password is provided
+//         const { firstName, lastName, username, password, role } = req.body;
+//         if (firstName && lastName && username && password && role) {
+//             try {
+//                 const user = new User({
+//                     firstName,
+//                     lastName,
+//                     username,
+//                     password,
+//                     role
+//                 });
+//                 // Create new user
+//                 var usercreated = await user.save();
+//                 console.log('user created');
+//                 return res.status(200).send(usercreated);
+//             } catch (error) {
+//                 console.log('error occured');
+//                 return res.status(500).send(error.message);
+//             }
+//         } else {
+//             res.status(422).send('data_incomplete');
+//         }
+//     }
+//     else {
+//         res.status(422).send('req_method_not_supported');
+//     }
+// };
+
+const handler = async (req, res) => {
     if (req.method === 'POST') {
-        // Check if name, email or password is provided
-        const { firstName, lastName, username, password, role } = req.body;
-        if (firstName && lastName && username && password && role) {
-            try {
-                const user = new User({
-                    firstName,
-                    lastName,
-                    username,
-                    password,
-                    role
-                });
-                // Create new user
-                var usercreated = await user.save();
-                console.log('user created');
-                return res.status(200).send(usercreated);
-            } catch (error) {
-                console.log('error occured');
-                return res.status(500).send(error.message);
-            }
-        } else {
-            res.status(422).send('data_incomplete');
+        //const { firstName, lastName, username, password, role } = req.body;
+        const userExist = await User.findOne({username: req.body.username});
+
+        if(userExist){
+            return res.status(500).send('user already exist')
         }
+        
+        const user = new User(req.body);
+
+        await user.save(function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send(user);
+        })
     }
     else {
         res.status(422).send('req_method_not_supported');
     }
-};
+}
 
 export default connectDB(handler);
